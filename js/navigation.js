f81a0fd4837b3a0b0361088c6116e10037029b64
larsmaeder <mail@larsmaeder.ch>
2017-09-07 15:15:07 +0200

    adding accordion script

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,19 @@
 $(function(){
     $("#insert-nav").load("/portfolio/html/nav.html"); 
+    
+    var acc = document.getElementsByClassName("open-menu");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var navigation = this.nextElementSibling;
+    if (navigation.style.maxHeight){
+      navigation.style.maxHeight = null;
+    } else {
+      navigation.style.maxHeight = navigation.scrollHeight + "px";
+    }
+  }
+}
+    
 });
