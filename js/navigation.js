42b267aabb95c79e82d683a66e8d42ca9437c3c6
Lars Mäder <mail@larsmaeder.ch>
2017-09-13 23:09:59 +0200

    navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,16 @@
 $(function(){
     $("#insert-nav").load("/portfolio/html/nav.html"); 
     
+    
+    
+    $('.side-nav a').click(function(){
+
+        $('.side-nav .side-nav-active').removeClass('side-nav-active nav-color-grey'); // remove the class from the currently selected
+        $(this).addClass('side-nav-active'); // add the class to the newly clicked link
+
+    });
+    
+    
     var acc = document.getElementsByClassName("open-menu");
 var i;
 
@@ -16,15 +26,4 @@ for (i = 0; i < acc.length; i++) {
   }
 }
     
-});
-
-$(function(){
-
-    $('.side-nav a').click(function(){
-
-        $('.side-nav .side-nav-active').removeClass('side-nav-active'); // remove the class from the currently selected
-        $(this).addClass('side-nav-active'); // add the class to the newly clicked link
-
-    });
-
 });
