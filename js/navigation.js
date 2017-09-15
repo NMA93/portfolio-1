13ac21a7cebdcb20411adc6e7a4b4541e6b97951
Lars Mäder <mail@larsmaeder.ch>
2017-09-15 16:17:13 +0200

    Change in Side-Nav

--- js/navigation.js
+++ js/navigation.js
@@ -1,30 +1,30 @@
 $(function(){
-    $("#insert-nav").load("/portfolio/html/nav.html"); 
+    
+    // Loading Main-Navigation in index.html
+    $("#insert-nav").load("./html/nav.html"); 
     
     
-    
+    // Activ function for Side-Nav
     $('.side-nav a').click(function(){
-
-        $('.side-nav .side-nav-active').removeClass('side-nav-active nav-color-grey'); // remove the class from the currently selected
-        $(this).addClass('side-nav-active li.nav-color-grey:before'); // add the class to the newly clicked link
-
+        $('.side-nav .side-nav-active').removeClass('side-nav-active'); // remove the class from the currently selected
+        $(this).addClass('side-nav-active'); // add the class to the newly clicked link
     });
     
     
+    // Accordion function for Main-Nav
     var acc = document.getElementsByClassName("open-menu");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    var navigationouter = this.nextElementSibling;
-    if (navigationouter.style.maxHeight){
-      navigationouter.style.maxHeight = null;
-    } else {
-      navigationouter.style.maxHeight = navigationouter.scrollHeight + "px";
-    }
-  }
-}
+    var i;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+            this.classList.toggle("active");
+            var navigationouter = this.nextElementSibling;
+            if (navigationouter.style.maxHeight){
+        navigationouter.style.maxHeight = null;
+            } else {
+        navigationouter.style.maxHeight = navigationouter.scrollHeight + "px";
+            }
+        }
+    }   
     
 });
 
